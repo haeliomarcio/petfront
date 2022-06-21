173f19636abd7063013c05e9f972764a5cda2266
Márcio Ferreira <profhaeliomarcio@gmail.com>
2022-06-20 21:59:58 -0300

    Autenticação

---
+++ js/load.js
@@ -0,0 +1,12 @@
+
+// ! NEGAÇÃO - UTILIZADO PARA NEGAR UMA VARIÁVEL
+const token = localStorage.getItem("token_petfront");
+
+if(!token) {
+    window.location = "../index.html";
+} 
+
+function logout() {
+    localStorage.removeItem("token_petfront");
+    window.location = "../index.html";
+}
